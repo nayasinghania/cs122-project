f4a586a859eee87dda9b6a2bcf145fdedf4e6ded
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2026-07-28 13:05:58 -0700

    basic UI + API calls

--- app.py
+++ app.py
@@ -1,9 +1,55 @@
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
+import tkinter.font as tkfont
+import os
+from cities import save_data, cloud_type
+import json
+import pandas as pd
+
+
+def on_close():
+    if os.path.exists("weather.csv"):
+        os.remove("weather.csv")
+    root.destroy()
+
+
+def submit():
+    city = city_var.get()
+    save_data(city)
+
+
+def search_location():
+    frame = tk.Frame(root)
+    entry = tk.Entry(frame, textvariable=city_var)
+    button = tk.Button(frame, text="Enter", command=submit)
+
+    frame.pack()
+    entry.grid(row=0, column=0)
+    button.grid(row=0, column=1)
+
+
+def open_popup():
+    with open("weather.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    curr = data["current"]
+    popup = tk.Toplevel(root)
+    popup.geometry("400x300")
+    popup.title("Current Weather")
+    city = city_var.get()
+    label = tk.Label(popup, text="Current Weather in " + city)
+    temp = tk.Label(
+        popup,
+        text=str(curr["temperature_2m"])
+        + " degrees and "
+        + cloud_type(curr["cloud_cover"]),
+    )
+    label.pack()
+    temp.pack()
+
 
 def plot():
-    fig = Figure(figsize = (5,5), dpi=100)
+    fig = Figure(figsize=(5, 5), dpi=100)
     y = [i**2 for i in range(101)]
     plot1 = fig.add_subplot(111)
     plot1.plot(y)
@@ -11,29 +57,28 @@ def plot():
     canvas.draw()
     canvas.get_tk_widget().pack()
 
-options = ['7 days', '30 days', '1 year']
+
+
+options = ["7 days", "30 days", "1 year"]
 
 root = tk.Tk()
-root.title('Weather Analysis (CS122 Project)')
-root.geometry('800x600')
+root.title("Weather Analysis (CS122 Project)")
+root.geometry("800x600")
+root.protocol("WM_DELETE_WINDOW", on_close)
 
-frame = tk.Frame(root)
-entry = tk.Entry(frame)
-button = tk.Button(frame, text='Enter')
+city_var = tk.StringVar()
 
-popup_button = tk.Button(text='Show Current Weather')
+search_location()
+
+popup_button = tk.Button(text="Show Current Weather", command=open_popup)
 
 dropdown_zoom = tk.Frame(root)
-selected = tk.StringVar(value='7 days')
+selected = tk.StringVar(value="7 days")
 dropdown = tk.OptionMenu(dropdown_zoom, selected, *options)
-zoom_in = tk.Button(dropdown_zoom, text='+')
-zoom_out = tk.Button(dropdown_zoom, text='-')
+zoom_in = tk.Button(dropdown_zoom, text="+")
+zoom_out = tk.Button(dropdown_zoom, text="-")
 
-save_button = tk.Button(text='Save plot as PNG')
-
-frame.pack()
-entry.grid(row=0, column=0)
-button.grid(row=0, column=1)
+save_button = tk.Button(text="Save plot as PNG")
 
 popup_button.pack()
 
---
+++ cities.py
@@ -0,0 +1,109 @@
+import requests
+import csv
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+import json
+
+cities = {
+    # lat, lon
+    "New York City": [40.7128, -74.0060],
+    "Los Angeles": [34.0522, -118.2437],
+    "Chicago": [41.8781, -87.6298],
+}
+
+
+def cloud_type(percent):
+    if percent < 5:
+        return "clear"
+    if percent < 10:
+        return "mostly clear"
+    if percent < 25:
+        return "slightly cloudy"
+    if percent < 50:
+        return "partly cloudy"
+    if percent < 75:
+        return "mostly cloudy"
+    return "overcast"
+
+
+def save_data(loc):
+    url = "https://historical-forecast-api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": cities[loc][0],
+        "longitude": cities[loc][1],
+        "daily": "temperature_2m_max,temperature_2m_min",
+        "timezone": "America/Los_Angeles",
+        "temperature_unit": "fahrenheit",
+        "start_date": (
+            datetime.now(ZoneInfo("America/Los_Angeles")) - timedelta(days=366)
+        ).strftime("%Y-%m-%d"),
+        "end_date": (
+            datetime.now(ZoneInfo("America/Los_Angeles")) - timedelta(days=1)
+        ).strftime("%Y-%m-%d"),
+    }
+    print(params)
+    response = requests.get(url, params=params, timeout=30)
+    response.raise_for_status()
+
+    data = response.json()
+    cleaned = data["daily"]
+
+    with open("weather.csv", "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["date", "temp_min", "temp_max"])
+        writer.writerows(
+            zip(
+                cleaned["time"],
+                cleaned["temperature_2m_max"],
+                cleaned["temperature_2m_min"],
+            )
+        )
+
+
+def current_weather(loc):
+    url = "https://api.open-meteo.com/v1/gfs"
+
+    params = {
+        "latitude": cities[loc][0],
+        "longitude": cities[loc][1],
+        "current": (
+            "temperature_2m,"
+            "wind_speed_10m,"
+            "wind_direction_10m,"
+            "wind_gusts_10m,"
+            "cloud_cover,"
+            "relative_humidity_2m,"
+            "precipitation,"
+            "rain,"
+            "showers,"
+            "snowfall,"
+            "surface_pressure"
+        ),
+        "temperature_unit": "fahrenheit",
+        "wind_speed_unit": "mph",
+        "precipitation_unit": "inch",
+        "forecast_days": 1,
+    }
+
+    response = requests.get(url, params=params, timeout=30)
+    response.raise_for_status()
+
+    data = response.json()
+
+    result = {
+        "city": loc,
+        "latitude": cities[loc][0],
+        "longitude": cities[loc][1],
+        "current": data["current"],
+        "current_units": data.get("current_units", {}),
+    }
+
+    filename = "weather.json"
+
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(result, file, indent=4)
+
+    return result
+
+
+weather = current_weather("Los Angeles")
